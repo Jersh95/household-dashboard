d90f5aa53536560ff1c06608b9047067008f6070
Jersh <joshajacobsen@gmail.com>
2023-03-30 19:26:39 -0500

    Implement React Auth to use Google Authentication

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Button } from 'react-bootstrap'
-import Link from 'next/link'
 import PageLayout from '@/components/Layout/PageLayout'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <PageLayout title='Split'>
-
+      
     </PageLayout>
   )
 }
